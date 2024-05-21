8b6b8f8b79494de081eacf0baa8901bb98e2a6f6
oscar <oscar@LAPTOP-G1N9MG0D>
2024-05-20 22:00:21 -0700

    added readme

---
+++ COMP003B.FinalProject/ReadMe/secret.cs
@@ -0,0 +1,12 @@
+﻿namespace COMP003B.FinalProject.ReadMe
+{
+    public class secret
+    {
+        /*
+        {
+  "ConnectionStrings": {
+    "DefaultConnection": "Server=(localdb)\\mssqllocaldb;Database=FitDb;Trusted_Connection=True;MultipleActiveResultSets=true"
+        */
+  }
+}
+
